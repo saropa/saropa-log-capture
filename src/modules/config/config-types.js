d342f5dbfd8ad828d6804222eb73ed846cec362a
saropa <53627075+saropa@users.noreply.github.com>
2026-03-26 18:09:50 -0400

    refactor(config): split types into integration and project-index modules; add normalizers

--- src/modules/config/config-types.js
+++ src/modules/config/config-types.js
@@ -1,9 +1,4 @@
 "use strict";
-/**
- * Configuration types and default highlight rules for Saropa Log Capture.
- * SaropaLogCaptureConfig is the single typed shape returned by getConfig(); integration
- * and project-index sub-shapes are defined here and merged in config.ts.
- */
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.defaultHighlightRules = void 0;
 var config_default_highlight_rules_1 = require("./config-default-highlight-rules");
